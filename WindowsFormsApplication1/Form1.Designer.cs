d7e699905a3089297965daec2f6b56e6b7ac9878
Hayato Nomura <h.nomura@sub.work-robotics.co.jp>
2023-10-02 14:19:10 +0900

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.idNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.posNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.freeButton = new System.Windows.Forms.Button();
+            this.normalButton = new System.Windows.Forms.Button();
+            this.setPosButton = new System.Windows.Forms.Button();
+            this.getPosButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.idNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.posNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 1500000;
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.ReadTimeout = 500;
+            // 
+            // idNumericUpDown
+            // 
+            this.idNumericUpDown.Location = new System.Drawing.Point(12, 37);
+            this.idNumericUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.idNumericUpDown.Name = "idNumericUpDown";
+            this.idNumericUpDown.Size = new System.Drawing.Size(75, 19);
+            this.idNumericUpDown.TabIndex = 0;
+            this.idNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // posNumericUpDown
+            // 
+            this.posNumericUpDown.Location = new System.Drawing.Point(174, 37);
+            this.posNumericUpDown.Maximum = new decimal(new int[] {
+            32767,
+            0,
+            0,
+            0});
+            this.posNumericUpDown.Minimum = new decimal(new int[] {
+            32768,
+            0,
+            0,
+            -2147483648});
+            this.posNumericUpDown.Name = "posNumericUpDown";
+            this.posNumericUpDown.Size = new System.Drawing.Size(77, 19);
+            this.posNumericUpDown.TabIndex = 1;
+            this.posNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Position";
+            // 
+            // freeButton
+            // 
+            this.freeButton.Location = new System.Drawing.Point(12, 62);
+            this.freeButton.Name = "freeButton";
+            this.freeButton.Size = new System.Drawing.Size(75, 45);
+            this.freeButton.TabIndex = 4;
+            this.freeButton.Text = "Free";
+            this.freeButton.UseVisualStyleBackColor = true;
+            this.freeButton.Click += new System.EventHandler(this.freeButton_Click);
+            // 
+            // normalButton
+            // 
+            this.normalButton.Location = new System.Drawing.Point(93, 62);
+            this.normalButton.Name = "normalButton";
+            this.normalButton.Size = new System.Drawing.Size(75, 45);
+            this.normalButton.TabIndex = 5;
+            this.normalButton.Text = "Normal";
+            this.normalButton.UseVisualStyleBackColor = true;
+            this.normalButton.Click += new System.EventHandler(this.normalButton_Click);
+            // 
+            // setPosButton
+            // 
+            this.setPosButton.Location = new System.Drawing.Point(174, 62);
+            this.setPosButton.Name = "setPosButton";
+            this.setPosButton.Size = new System.Drawing.Size(75, 45);
+            this.setPosButton.TabIndex = 6;
+            this.setPosButton.Text = "SetPos";
+            this.setPosButton.UseVisualStyleBackColor = true;
+            this.setPosButton.Click += new System.EventHandler(this.setPosButton_Click);
+            // 
+            // getPosButton
+            // 
+            this.getPosButton.Location = new System.Drawing.Point(255, 62);
+            this.getPosButton.Name = "getPosButton";
+            this.getPosButton.Size = new System.Drawing.Size(75, 45);
+            this.getPosButton.TabIndex = 7;
+            this.getPosButton.Text = "getPos";
+            this.getPosButton.UseVisualStyleBackColor = true;
+            this.getPosButton.Click += new System.EventHandler(this.getPosButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(366, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 19);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // serialPort2
+            // 
+            this.serialPort2.BaudRate = 115200;
+            this.serialPort2.PortName = "COM4";
+            this.serialPort2.ReadTimeout = 1;
+            this.serialPort2.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort2_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 121);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.getPosButton);
+            this.Controls.Add(this.setPosButton);
+            this.Controls.Add(this.normalButton);
+            this.Controls.Add(this.freeButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.posNumericUpDown);
+            this.Controls.Add(this.idNumericUpDown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.InputLanguageChanging += new System.Windows.Forms.InputLanguageChangingEventHandler(this.Form1_InputLanguageChanging);
+            ((System.ComponentModel.ISupportInitialize)(this.idNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.posNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.NumericUpDown idNumericUpDown;
+        private System.Windows.Forms.NumericUpDown posNumericUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button freeButton;
+        private System.Windows.Forms.Button normalButton;
+        private System.Windows.Forms.Button setPosButton;
+        private System.Windows.Forms.Button getPosButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+using Extensions.Collections;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public string str;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// フォームが立ち上がった時に呼ばれるイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open();             //COMポートを開く
+                serialPort2.Open();
+            }
+            catch
+            {
+                MessageBox.Show("COMが開けません！！");
+                /// COMが開けない場合は、SerialPortのCOM番号をチェックします
+                /// もしくは他のソフトウェアでSerialPortが占有されていないか確認します
+                serialPort1.Close();
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// フォームが閉じらられそうになった時に呼ばれるイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort1.Close();
+            serialPort2.Close();
+        }
+
+        #region B3Mの動作関数
+
+        /// <summary>
+        /// トルクオン、位置制御モード等々一度に設定する
+        /// </summary>
+        /// <param name="serialPort"></param>
+        /// <param name="servoID"></param>
+        /// <returns></returns>
+        private bool b3mModeSet(SerialPort serialPort, byte servoID, byte mode)
+        {
+            ByteList cmd = new ByteList();  //コマンド格納用
+            byte[] rx = new byte[5];        //コマンド受信用(Writeコマンドは5byte)
+
+            //コマンドの作成
+            //cmd.Bytes = B3MLib.B3MLib.Write(0, B3MLib.B3MLib.SERVO_TORQUE_ON, 1, servoID, new byte[] { mode });
+            cmd.Bytes = B3MLib.B3MLib.WriteSingle(0, B3MLib.B3MLib.SERVO_TORQUE_ON, servoID, new byte[] { mode });
+            
+            //option:0
+            //address:B3MLib.B3MLib.SERVO_TORQUE_ON (0x28)
+            //count:1(データを送るB3Mの数)
+            //ID:servoID
+            //data:送信するデータ配列
+
+            // コマンドを送信
+            return B3MLib.B3MLib.Synchronize(serialPort, cmd.Bytes,ref rx);
+        }
+
+
+        /// <summary>
+        /// Nomal、位置制御モードにする
+        /// </summary>
+        /// <param name="serialPort1">serialport</param>
+        /// <param name="servoID">id</param>
+        /// <returns></returns>
+        private bool b3mNomalPosModeSet(SerialPort serialPort, byte servoID)
+        {
+            return b3mModeSet(serialPort, servoID, ((byte)B3MLib.B3MLib.Options.ControlPosition | (byte)B3MLib.B3MLib.Options.RunNormal));
+        }
+
+        /// <summary>
+        /// Free、位置制御モードにする
+        /// </summary>
+        /// <param name="serialPort1">serialport</param>
+        /// <param name="servoID">id</param>
+        /// <returns></returns>
+        private bool b3mFreePosModeSet(SerialPort serialPort, byte servoID)
+        {
+            return b3mModeSet(serialPort, servoID, ((byte)B3MLib.B3MLib.Options.ControlPosition | (byte)B3MLib.B3MLib.Options.RunFree));
+        }
+
+        /// <summary>
+        /// 角度指定をする関数
+        /// </summary>
+        /// <param name="serialPort"></param>
+        /// <param name="servoID"></param>
+        /// <param name="angle"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        private bool b3mSetPosition(SerialPort serialPort, byte servoID, int angle, ushort time)
+        {
+            int seu = 5000;
+            ByteList cmd = new ByteList();  //コマンド格納用
+            byte[] rx = new byte[7];        //コマンド受信用(Writeコマンドは7byte)
+
+            //コマンドの作成
+            while (seu >= -5000)
+            {
+                seu=seu-10;
+                cmd.Bytes = B3MLib.B3MLib.SetPosision(0, servoID, seu, time);
+                B3MLib.B3MLib.Synchronize(serialPort, cmd.Bytes, ref rx);
+                this.Invoke(new EventHandler(textBox1_TextChanged));
+                Thread.Sleep(1);
+               
+            }
+           
+            //option:0
+            //ID:servoID
+            //pos:送信するデータ配列
+            //time:軌道生成時の時間
+
+            // コマンドを送信
+            return B3MLib.B3MLib.Synchronize(serialPort, cmd.Bytes, ref rx);
+        }
+
+
+        /// <summary>
+        /// サーボの現在角度を読み込む
+        /// </summary>
+        /// <param name="servoID"></param>
+        /// <param name="angle"></param>
+        /// <returns></returns>
+        static public bool b3mAngleRead(SerialPort serialPort, byte servoID, ref int angle)
+        {
+            ByteList cmd = new ByteList();
+            byte[] rx = new byte[7];        //コマンド受信用(Readで2byte受け取るコマンドは7byte)
+
+            //コマンドの作成
+            cmd.Bytes = B3MLib.B3MLib.Read(0x00, B3MLib.B3MLib.SERVO_CURRENT_POSITION, 2, servoID);
+            //option:0
+            //address:B3MLib.B3MLib.SERVO_CURRENT_POSITION (0x2C)
+            //count:2(受け取るデータの数)
+            //ID:servoID
+
+
+            // コマンドを送信
+            if (B3MLib.B3MLib.Synchronize(serialPort, cmd.Bytes, ref rx) == false)
+            {
+                return false;
+            }
+
+            //取得したデータをint(short)型に変換
+            angle = (short)Extensions.Converter.ByteConverter.ByteArrayToInt16(rx[4], rx[5]);
+
+            return true;
+        }
+
+
+        #endregion
+
+        #region ボタンの操作
+
+        /// <summary>
+        /// フリーボタンがクリックされたとき呼ばれる関数イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void freeButton_Click(object sender, EventArgs e)
+        {
+            bool flag;
+
+            flag = b3mFreePosModeSet(serialPort1,(byte)idNumericUpDown.Value);
+
+            if (flag == false)
+            {
+                MessageBox.Show("データの送信に失敗しました");
+            }
+
+        }
+        /// <summary>
+        /// ノーマルボタンがクリックされたときに呼ばれるイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void normalButton_Click(object sender, EventArgs e)
+        {
+            bool flag;
+
+            flag = b3mNomalPosModeSet(serialPort1, (byte)idNumericUpDown.Value);
+
+            if (flag == false)
+            {
+                MessageBox.Show("データの送信に失敗しました");
+            }
+        }
+
+        /// <summary>
+        /// setPosボタンが押されたとき
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void setPosButton_Click(object sender, EventArgs e)
+        {
+            bool flag;
+
+            flag = b3mSetPosition(serialPort1, (byte)idNumericUpDown.Value,(int)posNumericUpDown.Value,0);
+
+            if (flag == false)
+            {
+                MessageBox.Show("データの送信に失敗しました");
+            }
+        }
+
+        /// <summary>
+        /// getPosボタンが押されたとき
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void getPosButton_Click(object sender, EventArgs e)
+        {
+            bool flag;
+
+            int angle = new int();
+
+            flag = b3mAngleRead(serialPort1, (byte)idNumericUpDown.Value, ref angle);
+
+            
+            if (flag == false)
+            {
+                MessageBox.Show("データの送信に失敗しました");
+                return;
+            }
+
+            posNumericUpDown.Value = angle;
+        }
+
+        #endregion
+
+        private void Form1_InputLanguageChanging(object sender, InputLanguageChangingEventArgs e)
+        {
+
+        }
+
+        private void serialPort2_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            str = serialPort2.ReadLine();
+            Thread.Sleep(1);
+            this.Invoke(new EventHandler(textBox1_TextChanged));
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = str;
+        }
+    }
+}
